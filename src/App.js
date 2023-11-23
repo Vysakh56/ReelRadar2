7cf7b95d13c4e2bd8419300f8c119c05a0a5026f
Vysakh Venugopalan <vysakhvenugopalan@gmail.com>
2023-11-23 15:13:08 +0530

    Changed http to https for deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieCard from "./MovieCard.jsx";
 
 
 
-const API_URL=`http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+const API_URL=`https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`
 
 
 
